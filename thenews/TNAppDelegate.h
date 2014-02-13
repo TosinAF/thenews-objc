292a7b5a8efbd6e2bf6b8df8697bf7f36bdfbe5d
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-02-13 22:23:59 +0000

    Initial Commit

---
+++ thenews/TNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TNAppDelegate.h
+//  thenews
+//
+//  Created by Tosin Afolabi on 13/02/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ thenews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  thenews
+//
+//  Created by Tosin Afolabi on 13/02/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TNAppDelegate class]));
+    }
+}
