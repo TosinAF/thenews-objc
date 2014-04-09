8b7c1934120aaad1bd15769d35ac6451d63ce490
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-04-09 16:42:08 +0100

    Improved login page & added hn login

--- thenews/Controllers/TNLoginViewController.m
+++ thenews/Controllers/TNLoginViewController.m
@@ -6,21 +6,23 @@
 //  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
 //
 
+#import "libHN.h"
 #import "TNTextField.h"
+#import "TNNotification.h"
 #import "TNHomeViewController.h"
 #import "DesignerNewsAPIClient.h"
 #import "TNLoginViewController.h"
 
+TNType currentAuthType;
+
 @interface TNLoginViewController ()
+
+@property (nonatomic) UIButton *addDN;
+@property (nonatomic) UIButton *addHN;
 
 @end
 
 @implementation TNLoginViewController
-
-- (UIStatusBarStyle)preferredStatusBarStyle
-{
-    return UIStatusBarStyleLightContent;
-}
 
 - (void)viewWillAppear:(BOOL)animated
 {
@@ -30,10 +32,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:YES];
+    currentAuthType = TNTypeDesignerNews;
+    [self.view setBackgroundColor:[UIColor whiteColor]];
 
     CGSize screenSize = self.view.bounds.size;
-
 
     [self setTitle:@"Log In"];
     [[UINavigationBar appearance] setBarTintColor:[UIColor tnColor]];
@@ -41,7 +43,11 @@
     [[UINavigationBar appearance] setTitleTextAttributes: @{NSFontAttributeName:[UIFont fontWithName:@"Avenir-Light" size:18.0f],
                                                             NSForegroundColorAttributeName:[UIColor whiteColor]}];
 
-    [self.view setBackgroundColor:[UIColor whiteColor]];
+    UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(pushHomeView)];
+
+    [[self navigationItem] setRightBarButtonItem:rightItem];
+
+
 
     // Setup Text Fields & Borders
 
@@ -49,6 +55,7 @@
     [self.emailField setDelegate:self];
     [self.emailField setPlaceholder:@"Email"];
     [self.emailField setTag:0];
+    [self.emailField setAutocapitalizationType:UITextAutocapitalizationTypeNone];
 
     self.passwordField = [[TNTextField alloc] initWithFrame:CGRectMake(50, 130, screenSize.width, 50)];
     [self.passwordField setDelegate:self];
@@ -56,6 +63,7 @@
     [self.passwordField setReturnKeyType:UIReturnKeyDone];
     [self.passwordField setSecureTextEntry:YES];
     [self.passwordField setTag:1];
+    [self.passwordField setAutocapitalizationType:UITextAutocapitalizationTypeNone];
 
     // Add borders
 
@@ -69,19 +77,53 @@
         [self.view addSubview:border];
     }
 
-    // Error Label
+    // Log In Buttons
 
-    self.errorLabel = ({
-        UILabel *errorLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 225, screenSize.width, 100)];
-        [errorLabel setText:invalidFieldErrorMessage];
-        [errorLabel setTextColor:[UIColor redColor]];
-        [errorLabel setTextAlignment:NSTextAlignmentCenter];
-        [errorLabel setFont:[UIFont fontWithName:@"Avenir-Medium" size:16.5f]];
-        errorLabel;
+    self.addDN = ({
+
+        UIButton *addDN = [UIButton buttonWithType:UIButtonTypeCustom];
+        [addDN setFrame:CGRectMake(20, 210, screenSize.width  - 40, 50)];
+        [addDN setTitle:@"Enter DN Credentials" forState:UIControlStateNormal];
+        [addDN setTitleColor:[UIColor dnColor] forState:UIControlStateNormal];
+
+        [[addDN titleLabel] setTextAlignment:NSTextAlignmentCenter];
+        [[addDN titleLabel] setFont:[UIFont fontWithName:@"Avenir-Medium" size:14]];
+        [[addDN layer] setBorderColor:[[UIColor dnColor] CGColor]];
+        [[addDN layer] setBorderWidth:1.0f];
+
+
+        [addDN setBackgroundImage:[self imageWithColor:[UIColor dnColor]] forState:UIControlStateSelected];
+        [addDN setTitle:@"DN Account Added" forState:UIControlStateSelected];
+        [addDN setTitleColor:[UIColor whiteColor] forState:UIControlStateSelected];
+
+        [addDN addTarget:self action:@selector(dnButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+        addDN;
+    });
+
+    self.addHN = ({
+        UIButton *addHN = [UIButton buttonWithType:UIButtonTypeCustom];
+        [addHN setFrame:CGRectMake(20, 280, screenSize.width - 40, 50)];
+        [addHN setTitle:@"Add HN Account" forState:UIControlStateNormal];
+        [addHN setTitleColor:[UIColor hnColor] forState:UIControlStateNormal];
+
+        [[addHN titleLabel] setTextAlignment:NSTextAlignmentCenter];
+        [[addHN titleLabel] setFont:[UIFont fontWithName:@"Avenir-Medium" size:14]];
+        [[addHN layer] setBorderColor:[[UIColor hnColor] CGColor]];
+        [[addHN layer] setBorderWidth:1.0f];
+
+
+        [addHN setBackgroundImage:[self imageWithColor:[UIColor hnColor]] forState:UIControlStateSelected];
+        [addHN setTitle:@"HN Account Added" forState:UIControlStateSelected];
+        [addHN setTitleColor:[UIColor whiteColor] forState:UIControlStateSelected];
+
+        [addHN addTarget:self action:@selector(hnButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+        addHN;
     });
 
     [self.view addSubview:self.emailField];
     [self.view addSubview:self.passwordField];
+    [self.view addSubview:self.addDN];
+    [self.view addSubview:self.addHN];
 }
 
 #pragma mark - UITextField Delegate Methods
@@ -97,29 +139,114 @@
             break;
 
         case 1:
-            [self validateLogin];
+            if (currentAuthType == TNTypeDesignerNews) {
+                [self dnLogin];
+            } else {
+                [self hnLogin];
+            }
             break;
     }
     
     return NO;
 }
 
-//text view began editing, hide error label
+#pragma mark - Button Actions
 
-- (void)validateLogin
+- (void)dnButtonClicked
+{
+    currentAuthType = TNTypeDesignerNews;
+    [self.addDN setTitle:@"Enter DN Credentials" forState:UIControlStateNormal];
+    [self.addHN setTitle:@"Add HN Account" forState:UIControlStateNormal];
+    [self.emailField becomeFirstResponder];
+}
+
+- (void)hnButtonClicked
+{
+    currentAuthType = TNTypeHackerNews;
+    [self.addHN setTitle:@"Enter HN Credentials" forState:UIControlStateNormal];
+    [self.addDN setTitle:@"Add DN Account" forState:UIControlStateNormal];
+    [self.emailField becomeFirstResponder];
+}
+
+#pragma mark - Network Methods
+
+- (void)dnLogin
 {
     DesignerNewsAPIClient *DNClient = [DesignerNewsAPIClient sharedClient];
 
     [DNClient authenticateUser:self.emailField.text password:self.passwordField.text success:^(NSString *accessToken) {
 
-        [self pushHomeView];
+        [self.addDN setSelected:YES];
+        [self.addDN setUserInteractionEnabled:NO];
+
+        if ([self userCompletedLoginForBoth]) {
+             [self performSelector:@selector(pushHomeView) withObject:nil afterDelay:0.5];
+        } else {
+            // click other button automatically
+            [self hnButtonClicked];
+        }
 
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
 
-        NSLog(@"%@", [[error userInfo] objectForKey:@"NSLocalizedDescription"]);
-        [self.view addSubview:self.errorLabel];
+        [self showAuthenticationError];
 
     }];
+}
+
+- (void)hnLogin
+{
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    [[HNManager sharedManager] loginWithUsername:self.emailField.text password:self.passwordField.text completion:^(HNUser *user){
+
+        if (user) {
+
+            [self.addHN setSelected:YES];
+            [self.addHN setUserInteractionEnabled:NO];
+
+
+            if ([self userCompletedLoginForBoth]) {
+                // add delay
+                [self performSelector:@selector(pushHomeView) withObject:nil afterDelay:0.4];
+            } else {
+                // click other button automatically
+                [self dnButtonClicked];
+            }
+
+        } else {
+
+            [self showAuthenticationError];
+        }
+
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+    }];
+}
+
+#pragma mark - Private Methods
+
+- (UIImage *)imageWithColor:(UIColor *)color {
+
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return image;
+}
+
+- (BOOL)userCompletedLoginForBoth
+{
+    return [[HNManager sharedManager] userIsLoggedIn] && [[DesignerNewsAPIClient sharedClient] isUserAuthenticated];
+}
+
+- (void)showAuthenticationError
+{
+    TNNotification *error = [[TNNotification alloc] init];
+    [error showFailureNotification:@"Username or Password is Incorrect." subtitle:nil];
 }
 
 @end
