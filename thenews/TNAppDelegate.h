0eb7144cf05a3418b5291fe5fcb8622f7d7f98a8
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-04-09 16:42:48 +0100

    added method to manage network activity

--- thenews/TNAppDelegate.h
+++ thenews/TNAppDelegate.h
@@ -12,4 +12,6 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+- (void)setNetworkActivityIndicatorVisible:(BOOL)setVisible;
+
 @end
--- thenews/TNAppDelegate.m
+++ thenews/TNAppDelegate.m
@@ -99,4 +99,20 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)setNetworkActivityIndicatorVisible:(BOOL)setVisible {
+    static NSInteger NumberOfCallsToSetVisible = 0;
+    if (setVisible)
+        NumberOfCallsToSetVisible++;
+    else
+        NumberOfCallsToSetVisible--;
+
+    // The assertion helps to find programmer errors in activity indicator management.
+    // Since a negative NumberOfCallsToSetVisible is not a fatal error,
+    // it should probably be removed from production code.
+    NSAssert(NumberOfCallsToSetVisible >= 0, @"Network Activity Indicator was asked to hide more often than shown");
+
+    // Display the indicator as long as our static counter is > 0.
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:(NumberOfCallsToSetVisible > 0)];
+}
+
 @end
