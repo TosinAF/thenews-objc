7bc332aa92693540df85f73509be3096f76b025b
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-04-26 18:40:18 +0100

    created dnsettings view

---
+++ thenews/Controllers/DNSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TNSettingsViewController.h
+//  The News
+//
+//  Created by Tosin Afolabi on 24/04/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNSettingsViewController : UIViewController
+
+@end
