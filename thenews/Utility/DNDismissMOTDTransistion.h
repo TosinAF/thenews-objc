89d1de8d48620ea342aa9e4d81899a8f1a34fc91
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-04-26 16:51:08 +0100

    added custom motd transitions

---
+++ thenews/Utility/DNDismissMOTDTransistion.h
@@ -0,0 +1,13 @@
+//
+//  DNDismissMOTDTransistion.h
+//  The News
+//
+//  Created by Tosin Afolabi on 26/04/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DNDismissMOTDTransistion : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
--- thenews/Utility/DNDismissMOTDTransistion.m
+++ thenews/Utility/DNDismissMOTDTransistion.m
@@ -1,36 +1,32 @@
 //
-//  DNPresentMOTDTransition.m
+//  DNDismissMOTDTransistion.m
 //  The News
 //
-//  Created by Tosin Afolabi on 22/04/2014.
+//  Created by Tosin Afolabi on 26/04/2014.
 //  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
 //
 
-#import "DNPresentMOTDTransition.h"
+#import "DNDismissMOTDTransistion.h"
 
-@implementation DNPresentMOTDTransition
+@implementation DNDismissMOTDTransistion
 
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
 
     UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    UIView *containerView = [transitionContext containerView];
 
-    fromVC.view.alpha = 0.3;
-    toVC.view.alpha = 0.0;
-
-    [toVC.view setFrame:containerView.bounds];
-    [containerView addSubview:toVC.view];
-
-    [UIView animateWithDuration:0.5 animations:^{
+    [UIView animateWithDuration:0.3 animations:^{
+        [fromVC.view setAlpha:0.0];
         [toVC.view setAlpha:1.0];
     } completion:^(BOOL finished) {
+        [fromVC.view removeFromSuperview];
         [transitionContext completeTransition:YES];
     }];
+
 }
 
 - (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
-    return 0.5;
+    return 0.3;
 }
 
 @end
